ac116c3dba019f64aeb6e55b33656ffc38b381da
danilbokatov-ship-it <danilbokatov@gmail.com>
2026-08-08 01:28:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rbxcrate.db'
+import os
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///rbxcrate.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
